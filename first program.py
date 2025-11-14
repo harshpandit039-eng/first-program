d15cc020baf5bec247b9087548c6058c7e318d43
Harshit shukla <harshpandit039@gmail.com>
2025-11-14 13:41:06 +0530

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,34 @@
+"""
+task 1; perform basic mathematical operations
+problem statement;
+1. take two numbers as input from the user
+2.perform the basic mathematical operations on these two numbers
+
+1.addition
+2.subtraction
+3. multiplication
+4. division
+"""
+
+
+
+# num1=int(input("Enter the first number: "))
+# num2=int(input("Enter the second number: "))
+# print("addition =",num1+num2)
+# print("subtraction =",num1-num2)
+# print("multiplication =",num1*num2)
+# print("division =",num1/num2)
+
+
+"""
+task 2;create a personalized greeting.
+problem statement;
+
+1.take a user's first name and last name as input.
+2.concatenates the first name and last name into full name.
+3.prints personalized greeting message using the full name.
+
+"""
+# first_name=input("Enter your first name: ")
+# last_name=input("Enter your last name: ")
+# print("hello ," + first_name + " " + last_name,"welcome to the python program .")
