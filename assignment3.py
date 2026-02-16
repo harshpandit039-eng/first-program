614d780f4f32aedff7fee6689f06806a5d202957
Harshit shukla <harshpandit039@gmail.com>
2026-02-16 20:19:42 +0530

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -14,18 +14,3 @@ result=function(num)
 print(result)
 
 
-"""
-using the man module for calculations
-"""
-import math as m
-
-num=int(input("Enter a number: "))
-
-res1=m.sqrt(num)
-print(f"square root: {res1}")
-
-res2=m.log(num)
-print(f"logarithm :{res2}")
-
-res3=m.sin(num)
-print(f"sine :{res3}")
